7e465165a6f008b8b61985ce96fb604c54670d22
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-28 17:16:48 +0600

    slider add

--- src/Pages/Sheard/Navbar/Navbar.jsx
+++ src/Pages/Sheard/Navbar/Navbar.jsx
@@ -2,8 +2,15 @@ import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.svg'
 
 const Navbar = () => {
+
+    const navItem = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">About</Link></li>
+        <li><Link to="/">Service</Link></li>
+    </>
+
     return (
-        <div>
+        <div className='h-28'>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -24,36 +31,18 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {navItem}
                         </ul>
                     </div>
                     <Link to="/" className=""><img src={logo} alt="" /></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navItem}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn btn-error">Button</a>
                 </div>
             </div>
         </div>
