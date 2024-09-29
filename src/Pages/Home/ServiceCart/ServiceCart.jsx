9353f544ad03c13023bdb3c635805f2feb9471f6
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-29 14:26:59 +0600

    correction of cart

--- src/Pages/Home/ServiceCart/ServiceCart.jsx
+++ src/Pages/Home/ServiceCart/ServiceCart.jsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 
-const ServiceCart = ({service}) => {
-    const {title, img , price} = service;
+const ServiceCart = ({ service }) => {
+    const { _id, title, img, price } = service;
     return (
         <div>
             <div className="card bg-base-100 h-96 w-96 shadow-xl">
@@ -13,7 +14,9 @@ const ServiceCart = ({service}) => {
                     <h2 className="card-title">{title}</h2>
                     <p>Price : ${price}</p>
                     <div className="card-actions ">
-                        <button className="btn btn-error">Buy Now</button>
+                        <Link to={`checkout/${_id}`}>
+                            <button className="btn btn-error">Book Now</button>
+                        </Link>
                     </div>
                 </div>
             </div>
